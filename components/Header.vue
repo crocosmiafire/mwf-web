d3d6f2f6b31a0cbe8294276e20a6d62244fc1305
kawuchuu <kawuchuu@users.noreply.github.com>
2021-06-23 15:08:25 +1000

    did stuff

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="header-root">
+  <div class="header-root" :class="scrollBG">
     <section class="left">
       <h3>Melbourne Winter Fest</h3>
     </section>
@@ -15,6 +15,34 @@
   </div>
 </template>
 
+<script>
+export default {
+  data () {
+    return {
+      isScroll: false
+    }
+  },
+  computed: {
+    scrollBG () {
+      if (this.isScroll) {
+        return 'scroll'
+      } else {
+        return ''
+      }
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        this.isScroll = true
+      } else {
+        this.isScroll = false
+      }
+    })
+  }
+}
+</script>
+
 <style lang="scss" scoped>
 .header-root {
   width: 100%;
@@ -27,6 +55,21 @@
   justify-content: space-between;
   align-items: center;
   color: white;
+  z-index: 2;
+
+  transition: 0.25s;
+  transition-property: color, background;
+}
+
+.header-root.scroll {
+  background: white;
+  color: black;
+
+  box-shadow: 0px 1px 5px rgba(0,0,0,.2);
+
+  a {
+    color: black;
+  }
 }
 
 .links {
--- pages/index.vue
+++ pages/index.vue
@@ -17,9 +17,67 @@
         </div>
       </div>
     </section>
-    <section>
-      <h1>About</h1>
+    <section class="about">
+      <svg
+        id="poly1"
+        width="0"
+        height="0"
+      >
+        <g>
+          <defs>
+            <clipPath id="poly1clip">
+              <polygon
+                id="SVGID_1_"
+                points="2243.18,1287.08 1977.84,1212.37 1746.08,1353.03 1501.67,1270.49 1269.91,1411.15
+                1025.49,1328.62 793.73,1469.28 549.31,1386.74 317.55,1527.4 73.13,1444.86 -180.16,1582.87 -118.66,1399.96 -225.99,1288.57
+                -132.68,1152.69 -240.01,1041.29 -146.7,905.41 -254.03,794.01 -160.72,658.13 -268.06,546.73 -174.75,410.84 -256.01,245.34
+                9.33,320.05 241.09,179.39 485.51,261.93 717.27,121.27 961.69,203.8 1193.44,63.15 1437.86,145.68 1669.62,5.02 1914.05,87.56
+                2167.33,-50.45 2105.83,132.46 2213.16,243.85 2119.85,379.73 2227.19,491.13 2133.88,627.01 2241.21,738.41 2147.9,874.29
+                2255.23,985.69 2162.33,1125.97"
+              />
+            </clipPath>
+          </defs>
+          <use xlink:href="#SVGID_1_" style="overflow:visible;fill:#FFFFFF;" />
+        </g>
+      </svg>
+      <div class="lmao">
+        <div class="about-inner" id="About">
+          <h1>The ultimate Melbourne festival</h1>
+          <p>
+            Experience Melbourne like you've never experienced it before.
+            From cafes and restaurants, to the Sidney Myer Music Bowl, everyone is celebrating this
+            uniquely wonderful city.
+          </p>
+        </div>
+        <div class="bg-thing" />
+      </div>
     </section>
+    <!-- <section class="schedule">
+      <svg
+        id="poly1"
+        width="0"
+        height="0"
+      >
+        <g>
+          <defs>
+            <clipPath id="poly2clip">
+              <polygon
+                id="SVGID_1_"
+                points="2160.8,1348.6 1906.5,1242.2 1659.4,1353.6 1426.8,1242 1179.7,1353.5 947.1,1241.9
+                699.9,1353.4 467.4,1241.7 220.2,1353.2 -12.4,1241.6 -280.5,1347.8 -197.3,1173.7 -290.3,1050.1 -181.2,926.6 -274.2,803
+                -165,679.4 -258,555.8 -148.9,432.3 -241.9,308.7 -132.8,185.1 -193.4,11 60.9,117.4 308.1,5.9 540.6,117.5 787.8,6 1020.4,117.6
+                1267.5,6.2 1500.1,117.8 1747.2,6.3 1979.8,117.9 2248,11.7 2164.7,185.8 2257.7,309.4 2148.6,432.9 2241.6,556.6 2132.5,680.1
+                2225.5,803.7 2116.4,927.2 2209.4,1050.9 2100.1,1178.8"
+              />
+            </clipPath>
+          </defs>
+          <use xlink:href="#SVGID_1_" style="overflow:visible;fill:#FFFFFF;" />
+        </g>
+      </svg>
+      <div class="schedule-inner">
+        <h1>yeah</h1>
+      </div>
+    </section> -->
   </div>
 </template>
 
@@ -30,11 +88,7 @@ export default {
       imgs: [
         'https://summersoundsfestival.com/content/uploads/sites/2/2021/01/138946391_3877321168999287_6162802858430709152_o-3000x0-c-default.jpg',
         'https://cdn.concreteplayground.com/content/uploads/2017/05/Queen-Victoria-Market-Winter-Night.jpeg',
-        'http://theluxtraveller.com/wp-content/uploads/2015/06/Melbourne-at-night.jpg',
-        'https://pbs.twimg.com/media/E4Q8F-EWYAAXPQO?format=jpg&name=medium',
-        'https://pbs.twimg.com/media/E4LwztyVkAEWaZE?format=jpg&name=900x900',
-        'https://pbs.twimg.com/media/E38LZh-VUAcUjZc?format=png&name=small',
-        'https://static.wikia.nocookie.net/minecraft_gamepedia/images/4/4a/Campfire_JE1_BE1.gif'
+        'http://theluxtraveller.com/wp-content/uploads/2015/06/Melbourne-at-night.jpg'
       ],
       curImg: 0
     }
@@ -58,10 +112,81 @@ export default {
   --header: 'Domine', 'Inter', sans-serif;
 }
 
+.about {
+  position: relative;
+  bottom: 225px;
+  z-index: 1;
+
+  .lmao {
+    width: 100%;
+    height: 1100px;
+    z-index: -1;
+    position: relative;
+
+    background-color: white;
+
+    clip-path: url(#poly1clip);
+
+    display: flex;
+    justify-content: space-between;
+
+    .about-inner {
+      padding: 360px 0px 0px 125px;
+      width: 50%;
+
+      transform: translateY(100px);
+    }
+
+    .bg-thing {
+      position: relative;
+      z-index: -1;
+
+      height: 100%;
+      width: 45%;
+
+      background-image: url('https://www.webjet.com.au/travel/wp-content/uploads/2014/07/Melbourne_at_night.jpg');
+      background-size: cover;
+      background-position: right;
+    }
+
+    h1 {
+      font-family: var(--header);
+      font-size: 4em;
+      letter-spacing: -0.03em;
+    }
+
+    p {
+      margin-top: 25px;
+      font-size: 1.25em;
+      padding-right: 20px;
+      line-height: 1.5em;
+    }
+  }
+}
+
+.schedule {
+  position: relative;
+  z-index: 1;
+
+  .schedule-inner {
+    width: 100%;
+    height: 500px;
+    background: black;
+    color: white;
+
+    padding: 350px 0px;
+
+    clip-path: url(#poly2clip);
+
+    display: flex;
+    justify-content: center;
+  }
+}
+
 .bg {
   position: absolute;
   width: 100%;
-  height: 600px;
+  height: 740px;
   z-index: -1;
   filter: brightness(0.3);
   top: 0;

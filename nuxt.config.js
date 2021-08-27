1ae7f8faaa119fa15ebb269b7a45554ddfb83919
crocosmiafire <crocosmiafire@users.noreply.github.com>
2021-08-27 10:12:45 +1000

    yeah

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'mwf',
+    title: 'Melbourne Winter Festival',
     htmlAttrs: {
       lang: 'en'
     },
@@ -44,5 +44,11 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  target: 'static',
+
+  router: {
+    base: '/'
   }
 }

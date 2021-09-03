aea79161895efbbffbf942906600057598e89f2d
crocosmiafire <crocosmiafire@users.noreply.github.com>
2021-09-03 14:29:26 +1000

    add pres stuff

--- pages/pres.vue
+++ pages/pres.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="pres-root">
-    <h1>lol</h1>
+    <img src="imgs/pres.png">
+    <a href="../">
+      <div class="button">
+        <span>Enter Website</span>
+      </div>
+    </a>
   </div>
 </template>
 
@@ -11,5 +16,51 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.pres-root {
+  display: grid;
+  grid-template-rows: 4fr 1fr;
+  justify-items: center;
+  align-items: center;
+  color: white;
 
+  img {
+    width: 1280px;
+    height: auto;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  .button {
+    padding: 15px 20px;
+    border: solid 6px;
+    width: 250px;
+    border-top-color: #E000FF;
+    border-right-color: #FFFF00;
+    border-bottom-color: #00D4FF;
+    border-left-color: white;
+    border-radius: 15px;
+    font-size: 1.4em;
+    cursor: pointer;
+    user-select: none;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: white;
+
+    i {
+      margin-left: 10px;
+    }
+  }
+  .button:hover {
+    background: #ffffff21;
+  }
+
+  .button:active {
+    background: #ffffff21;
+    opacity: 0.5;
+  }
+}
 </style>
